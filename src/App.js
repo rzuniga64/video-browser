00d6101c601c1f75065098b627917b30e8b350c9
Raul Zuniga <rzuniga64@gmail.com>
2017-07-08 01:55:52 -0500

    Add youtube-api-search.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBcYhW96BlmpdEZ_2IyLHwR9kh2fMmqRvU';
 
@@ -12,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+          <div class="search-bar"><SearchBar/></div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
